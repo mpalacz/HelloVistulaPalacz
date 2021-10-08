4ca7a4884730fc8776c3499ea51380dab4aa4004
student <student@lab.pl.wsei>
2021-10-08 11:56:52 +0200

    Initial commit

---
+++ src/pl/vistula/hellowrldnazwisko/HelloWorldPalacz.java
@@ -0,0 +1,13 @@
+package pl.vistula.hellowrldnazwisko;
+
+public class HelloWorldPalacz {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Vistula Palacz");
+        System.out.println("fm");
+
+        int x = 3;
+        int y = 7;
+        int w = x + y;
+    }
+}
